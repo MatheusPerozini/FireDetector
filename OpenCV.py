aacd1df26497aa481676e962f0a754aaf09049f1
Matheus Perozini Magalhães <perozinimagalhesmatheus@gmail.com>
2023-02-25 13:38:36 -0300

    Adicionado detecção de fumaça

--- OpenCV.py
+++ OpenCV.py
@@ -4,8 +4,7 @@ import os
 import os.path
 import cv2
 
-filename = '1.jpg'
-threshold = 200
+filename = '42.jpg'
 
 url = os.path.dirname(os.path.abspath(__file__))
 img = cv2.imread(url+'/Fire-Detection/1/'+filename)
@@ -16,39 +15,78 @@ if img is None:
 blur = cv2.GaussianBlur(img , (15 , 15) , 0)
 hsv = cv2.cvtColor(blur , cv2.COLOR_BGR2HSV)
 
+# Mascara fogo
 lowerFire = (0, 115, 155)
 upperFire = (30, 255, 255)
 
 lowerFire = np.array(lowerFire , dtype='uint8')
 upperFire = np.array(upperFire , dtype='uint8')
 
-mask = cv2.inRange(hsv , lowerFire , upperFire)
+maskFire = cv2.inRange(hsv , lowerFire , upperFire)
 
-outputFogo = cv2.bitwise_and(img , hsv , mask=mask)
+outputFogo = cv2.bitwise_and(img , hsv , mask=maskFire)
 
-tamanhoFogo = cv2.countNonZero(mask)
+tamanhoFogo = cv2.countNonZero(maskFire)
+print(tamanhoFogo)
 
-gray = cv2.cvtColor(outputFogo, cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(outputFogo,threshold,255,cv2.THRESH_BINARY)
-height, width = thresh.shape[:2]
+retFire,threshFire = cv2.threshold(outputFogo,30,255,cv2.THRESH_BINARY)
+heightFire, widthFire = threshFire.shape[:2]
 
-mass = 0
-Xcm  = 0.0
-Ycm  = 0.0
+massFire = 0
+XcmFire  = 0.0
+YcmFire  = 0.0
 
-for i in range(width) :
-    for j in range(height) :
-        if not (thresh[j][i]).any() :
-            mass += 1
-            Xcm  += i
-            Ycm  += j
+for i in range(widthFire) :
+    for j in range(heightFire) :
+        if not all(threshFire[j][i] == 0) :
+            massFire += 1
+            XcmFire  += i
+            YcmFire  += j
 
-Xcm = Xcm/mass
-Ycm = Ycm/mass
-fig = plt.figure()
-fig.clear()
-plot = fig.add_subplot(111)
-plot.imshow(thresh, 'gray')
-plot.scatter([Xcm], [Ycm], s=30, c='yellow', edgecolors='red')
-extent = plot.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-fig.savefig('imagem_final.png', bbox_inches=extent.expanded(1.2, 1.0))
+XcmFire = XcmFire/massFire
+YcmFire = YcmFire/massFire
+figFire = plt.figure()
+figFire.clear()
+plotFIre = figFire.add_subplot(111)
+plotFIre.imshow(threshFire, 'gray')
+plotFIre.scatter([XcmFire], [YcmFire], s=30, c='yellow', edgecolors='red')
+extentFire = plotFIre.get_window_extent().transformed(figFire.dpi_scale_trans.inverted())
+figFire.savefig('imagem_fogo.png', bbox_inches=extentFire.expanded(1.2, 1.0))
+
+# Mascara Fumaça
+lowerSmoke = (0, 0, 130)
+upperSmoke = (179, 50, 255)
+
+lowerSmoke = np.array(lowerSmoke , dtype='uint8')
+upperSmoke = np.array(upperSmoke , dtype='uint8')
+
+maskSmoke = cv2.inRange(hsv , lowerSmoke , upperSmoke)
+
+outputFumaça = cv2.bitwise_and(img , hsv , mask=maskSmoke)
+
+tamanhoFumaça = cv2.countNonZero(maskSmoke)
+print(tamanhoFumaça)
+
+retSmoke,threshSmoke = cv2.threshold(outputFumaça,30,255,cv2.THRESH_BINARY)
+heightSmoke, widthSmoke = threshSmoke.shape[:2]
+
+massSmoke = 0
+XcmSmoke  = 0.0
+YcmSmoke  = 0.0
+
+for i in range(widthSmoke) :
+    for j in range(heightSmoke) :
+        if not all(threshSmoke[j][i] == 0) :
+            massSmoke += 1
+            XcmSmoke  += i
+            YcmSmoke  += j
+
+XcmSmoke = XcmSmoke/massSmoke
+YcmSmoke = YcmSmoke/massSmoke
+figSmoke = plt.figure()
+figSmoke.clear()
+plotSmoke = figSmoke.add_subplot(111)
+plotSmoke.imshow(threshSmoke, 'gray')
+plotSmoke.scatter([XcmSmoke], [YcmSmoke], s=30, c='yellow', edgecolors='red')
+extent = plotSmoke.get_window_extent().transformed(figSmoke.dpi_scale_trans.inverted())
+figSmoke.savefig('imagem_fumaca.png', bbox_inches=extent.expanded(1.2, 1.0))
